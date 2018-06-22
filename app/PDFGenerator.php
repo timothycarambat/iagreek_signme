90842d6413463d9384f4e8e095ccb09b698164a9
timothycarambat <rambat1010@gmail.com>
2018-06-21 21:09:54 -0500

    show image on header

--- app/PDFGenerator.php
+++ app/PDFGenerator.php
@@ -5,27 +5,22 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use TCPDF;
 use Auth;
+use Storage;
 
 class PDF extends TCPDF {
 
     //Page header
     public function Header() {
         $letterhead_url = Auth::user()->org_admin->letterhead;
-        $image_file = $letterhead_url;
-        $this->Image($image_file, 10, 10, 15, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
-        // Set font
-        $this->SetFont('helvetica', 'B', 20);
-        // Title
-        $this->Cell(0, 15, '<< TCPDF Example 003 >>', 0, false, 'C', 0, '', 0, false, 'M', 'M');
+        $image_encoded = base64_encode(file_get_contents($letterhead_url));
+        $imgdata = base64_decode($image_encoded);
+        $this->Image('@'.$imgdata,10,10,50);
     }
 
     // Page footer
     public function Footer() {
-        // Position at 15 mm from bottom
         $this->SetY(-15);
-        // Set font
         $this->SetFont('helvetica', 'I', 8);
-        // Page number
         $this->Cell(0, 10, 'Page '.$this->getAliasNumPage().'/'.$this->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, 'T', 'M');
     }
 }
